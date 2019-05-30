febbf073c2800c45c4485a313a344145850799fa
Peter Jones <pjones@redhat.com>
2019-05-30 13:27:18 -0400

    libahkbahks: Add a central ctor/dtor function.
    This way we get to control the order all constructor and destructor
    functions fire in.
    
    Signed-off-by: Peter Jones <pjones@redhat.com>

---
+++ libahkbahks/constructor.c
@@ -0,0 +1,20 @@
+/*
+ * constructor.c
+ * Copyright 2019 Peter Jones <pjones@redhat.com>
+ *
+ */
+
+#include "ahkbahks.h"
+
+static void CONSTRUCTOR
+ahkbahks_ctor(void)
+{
+}
+
+static void DESTRUCTOR
+ahkbahks_dtor(void)
+{
+
+}
+
+// vim:fenc=utf-8:tw=75:noet
